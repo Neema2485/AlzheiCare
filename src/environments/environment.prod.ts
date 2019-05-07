ac08744fff9a8661acb3565e11c2e734eb1ffa81
Neema2485 <32157762+Neema2485@users.noreply.github.com>
2019-05-06 23:40:19 -0700

    env changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
-  production: true,
+  production: false,
   googleMapApiKey:"AIzaSyBzIro2tf6IiKET8mLqJg2K6US79UcMzSg",
   firebase:{
-    apiKey: "AIzaSyBzIro2tf6IiKET8mLqJg2K6US79UcMzSg",
-    authDomain: "app-alzheimer.firebaseapp.com",
-    databaseURL: "https://app-alzheimer.firebaseio.com",
-    projectId: "app-alzheimer",
-    storageBucket: "app-alzheimer.appspot.com",
-    messagingSenderId: "1067336857046"
+    apiKey: "AIzaSyBbmXjJPs6qqhS91O29boKggf3NwehlXdc",
+    authDomain: "app-alzheimer123.firebaseapp.com",
+    databaseURL: "https://app-alzheimer123.firebaseio.com",
+    projectId: "app-alzheimer123",
+    storageBucket: "app-alzheimer123.appspot.com",
+    messagingSenderId: "1002558619889"
   }
 };
